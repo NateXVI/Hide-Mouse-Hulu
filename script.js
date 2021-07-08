b523b55ded97ae93e4421b3ab68011811cb4718b
Nate Diven <natediven1@gmail.com>
2021-07-08 17:15:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let mouseTimer = null;
 
 function setMouse(state) {
     try {
-        document.querySelector('#web-player-app').style.cursor = state ? 'auto' : 'none';
+        document.querySelector('.class36, .class4').style.cursor = state ? 'auto' : 'none';
     } catch (error) {}
 }
 
 function resetTimer() {
     setMouse(1);
     if (mouseTimer) window.clearTimeout(mouseTimer);
-    mouseTimer = setTimeout(setMouse, 3200, 0);
+    mouseTimer = setTimeout(setMouse, 5000, 0);
 }
 
 window.addEventListener('mousemove', resetTimer)
