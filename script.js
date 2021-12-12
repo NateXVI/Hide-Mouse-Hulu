59a8fcc0431a175924c33207fbfe7a222492d49f
Nate Diven <natediven1@gmail.com>
2021-12-11 23:42:10 -0700

    Added comments and more listeners that reset the timer

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
+// variable to store the timer that hides the mouse
 let mouseTimer = null;
 
 function setMouse(state) {
-    try {
-        document.querySelector('#web-player-app').style.cursor = state ? 'auto' : 'none';
-    } catch (error) {}
+	// hides or shows the mouse depending on state given
+	try {
+		document.querySelector('#web-player-app').style.cursor = state ? 'auto' : 'none';
+	} catch (error) {}
 }
 
 function resetTimer() {
-    setMouse(1);
-    if (mouseTimer) window.clearTimeout(mouseTimer);
-    mouseTimer = setTimeout(setMouse, 3200, 0);
+	// resets the mouse timer and makes mouse visible
+	setMouse(1);
+	if (mouseTimer) window.clearTimeout(mouseTimer);
+	mouseTimer = setTimeout(setMouse, 3100, 0);
 }
 
-window.addEventListener('mousemove', resetTimer)
+// resets timer if the mouse is moved
+window.addEventListener('mousemove', resetTimer);
+
+// resets timer if the mouse is clicked
+window.addEventListener('mousedown', resetTimer);
+
+// keys that will reset the timer when pressed
+let listenKeys = [
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowLeft',
+	'ArrowRight',
+	' ',
+	'Tab',
+	'k',
+	'm',
+];
+
+// make all the key strings lowercase so uppercase keys reset timer as well
+listenKeys = listenKeys.map((key) => key.toLowerCase());
+
+window.addEventListener('keydown', (e) => {
+	// when a key is pressed...
+	if (listenKeys.includes(e.key.toLowerCase())) {
+		// if the key pressed is one of the listen keys, reset the timer
+		resetTimer();
+	}
+});
