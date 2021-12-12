d9c0b0646dddb90b87f3b0b7366dc3c984e821dd
Nate Diven <natediven1@gmail.com>
2021-12-12 03:21:01 -0700

    Trying out new way of hiding mouse
    The old version of this extension hides the mouse when it is idle and shows it when you move it. This works perfectly fine, but it does not alway match up with the visibility of the web player ui. This new algorithm mirrors the visibility of the cursor with the visibiliy of the controls.

--- script.js
+++ script.js
@@ -1,45 +1,57 @@
-// variable to store the timer that hides the mouse
-let mouseTimer = null;
+// controls container variable declaration
+let controlsContainer = null;
 
-function setMouse(state) {
-	// hides or shows the mouse depending on state given
+// delay between searches for controls container (in milliseconds)
+const findDelay = 500;
+
+function setMouse(visibility) {
+	// sets the mouse visibility for the Hulu web player
 	try {
-		document.querySelector('#web-player-app').style.cursor = state ? 'auto' : 'none';
-	} catch (error) {}
-}
-
-function resetTimer() {
-	// resets the mouse timer and makes mouse visible
-	setMouse(1);
-	if (mouseTimer) window.clearTimeout(mouseTimer);
-	mouseTimer = setTimeout(setMouse, 3100, 0);
-}
-
-// resets timer if the mouse is moved
-window.addEventListener('mousemove', resetTimer);
-
-// resets timer if the mouse is clicked
-window.addEventListener('mousedown', resetTimer);
-
-// keys that will reset the timer when pressed
-let listenKeys = [
-	'ArrowUp',
-	'ArrowDown',
-	'ArrowLeft',
-	'ArrowRight',
-	' ',
-	'Tab',
-	'k',
-	'm',
-];
-
-// make all the key strings lowercase so uppercase keys reset timer as well
-listenKeys = listenKeys.map((key) => key.toLowerCase());
-
-window.addEventListener('keydown', (e) => {
-	// when a key is pressed...
-	if (listenKeys.includes(e.key.toLowerCase())) {
-		// if the key pressed is one of the listen keys, reset the timer
-		resetTimer();
+		// set the web player element cursor style
+		document.querySelector('#web-player-app').style.cursor = visibility
+			? 'auto'
+			: 'none';
+	} catch (error) {
+		// if the mouse visibility cannot be set, log a warning in the console
+		console.warn('could not set mouse visibility', error);
 	}
-});
+}
+
+function findControlsContainer() {
+	// look for a controls container
+	controlsContainer = document.querySelector('.ControlsContainer__transition');
+
+	// if the controls container cannot be found
+	if (controlsContainer === null) {
+		// set timeout to look again in 500ms
+		window.setTimeout(findControlsContainer, findDelay);
+	}
+	// if the controls container can be found
+	else {
+		// initialize the observer
+		initializeControlsObserver();
+	}
+}
+
+function initializeControlsObserver() {
+	// creates an observer and sets it to look for changes in the controls container
+
+	function callback(mutations) {
+		// get the opacity of the controls container
+		let opacity = parseFloat(controlsContainer.style.opacity);
+
+		// make the mouse visible if the opacity is greater than 0.5
+		setMouse(opacity >= 0.5);
+	}
+
+	// create an observer
+	const observer = new MutationObserver(callback);
+
+	// set the observer to look for changes in the controls container
+	observer.observe(controlsContainer, {
+		attributes: true, //configure it to listen to attribute changes
+	});
+}
+
+// start finding the controls container
+findControlsContainer();
